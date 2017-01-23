38c63d38391a4673249afd67b4a6d4801daa9b1d
Ahmed Khalaf <pxlshpr@gmail.com>
2017-01-23 11:28:22 +0800

    start placing comments for unit tests

--- Style Generator/Style GeneratorTests/ColorViewControllerTests.swift
+++ Style Generator/Style GeneratorTests/ColorViewControllerTests.swift
@@ -4,13 +4,14 @@ class ColorViewControllerTests: XCTestCase {
     
     override func setUp() {
         super.setUp()
+        // setup a bunch of ColorViewController's here with a bunch of different paramerters (different set of colors; material-primary, material-secondary, just one or two, thousands, many of the same color 
     }
     
     override func tearDown() {
         super.tearDown()
     }
     
-    func testSomething() {
+    func testNumberOfCellsEqualsProvidedColorsCount() {
         XCTAssertTrue(true)
     }
 }
