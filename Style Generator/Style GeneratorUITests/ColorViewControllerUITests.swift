776ff7cbb8b73d9857c62e31218db5010d8aaead
Ahmed Khalaf <pxlshpr@gmail.com>
2017-01-26 11:04:51 +0800

    begin writing UI tests with proper naming convention

--- Style Generator/Style GeneratorUITests/ColorViewControllerUITests.swift
+++ Style Generator/Style GeneratorUITests/ColorViewControllerUITests.swift
@@ -15,73 +15,81 @@ class ColorViewControllerUITests: XCTestCase {
     super.tearDown()
   }
   
-  //TODO: replace with strings key for localization
-//  struct Strings {
-//    static let primaryColorTitle = "Primary Color"
-//    static let nextButtonTitle = "Next"
-//  }
-  
-//  func testInitialLoad() {
-//    // Check that the Navigation Bar:
-//    let navigationBar = app.navigationBars[Strings.primaryColorTitle]
-//    let nextButton = navigationBar.buttons[Strings.nextButtonTitle]
-//    
-//    XCTAssert(navigationBar.exists, "Navigation bar with title ")
-//    XCTAssert(nextButton.exists, "Next button doesn't exist")
-//    XCTAssertFalse(nextButton.isEnabled, "Next button isn't disabled initially")
-//    
-//    
-//    let color = navigationBar.value(forKey: "tintColor")
-//    
-//    XCTAssertTrue(true)
-//    // …tintColor and nextButton's tintColor is at the default
-//    
-//    // Check that the status bar is at its default tint
-//    // Check that there is one and only one collectionView, that:
-//    // …has the same number of cells as the number of primary colors in Realm (should we really do this?)
-//    // …some sort of sizing constraint with respect to UIBounds to ensure that an adequate number of cells are displayed at any given time
-//    
-//    // Check that each of the cells:
-//    // …have their own associated color value as the background-color
-//    // …have their labels hidden
-//  }
-//  
-  func testSelectingAndDeselectingAPrimaryColor() {
+  //UI Test naming convention:
+  //test[Action]_[WithCertainState]_[ShouldDoSomething]
+  //test[Action]_[ShouldDoSomething] // when we test it with all states
+  func testTapOnColorCell_ShouldSelectItAndDeselectAnyPreviousSelection() {
     
-    //TODO: do this a few times just for funm
-    //TODO: modularize code in here such as:
-    // …testing that the cell is in its 'unselected-state', i.e. label is hidde, background-color is that of associated value, etc.
-    // …testing that the screen is in a 'selected' or 'unselected' state perhaps – which would in turn use other modularized bits of test code
-    
-    // Tap a random cell
-    //TODO: figure out why we can put numbers in boundBy but not variables??
-//    let cells = XCUIApplication().collectionViews.children(matching: .cell)
-//    let randomCell = randomIntegerBetween0(and: Int(cells.count))
-    
-    XCUIApplication().collectionViews.children(matching: .cell).element(boundBy: 4).children(matching: .other).element.tap()
-    XCTAssertTrue(true)
-    
-    //    cells.element(boundBy: randomCell).children(matching: other).element.tap()
-    
-    //**** Check that these things (eventually) happen – give a max delay of say 5 seconds
-    
-    // Check that the navigation bar:
-    // …has changed color to the selected cell's color
-    // …has changed its title to the selected cell's color name
-    
-    // Check that the next button:
-    // …has become enabled
-    // …has changed its tintColor depending on the chosen color
-    
-    // Check that the tapped cell:
-    // …retains its selected sate
-    
-    // Check that all cells except the tapped cell (at least those that are in view):
-    // …changed their background-color to grey
-    // …made their title visible, with a white color
-    
-    //****
+    ensureNoCellsAreSelected()
+    tapTheFirstCell()
+    //ensure it was set
   }
+  
+  func tapTheFirstCell() {
+    let visibleCells = app.collectionViews.children(matching: .cell)
+    let randomIndex = randomIntegerBetween0(and: Int(visibleCells.count))
+    visibleCells.element(boundBy: UInt(randomCell)).tap()    
+  }
+  
+  //TODO: think about if the fact that we're only doing this with *visible* cells should be of concern?
+  func ensureNoCellsAreSelected() {
+  }
+  
+  func testInitialState() {
+    //TODO: move these all to unit-tests!
+    // Check that the Navigation Bar:
+    // ... title is correct
+    // ... has the default tintColor
+    
+    // Check that the status bar is at its default tint
+    
+    // Check that next button
+    // ... exists
+    // ... is initially disabled
+    // ... has the default tintColor
+    
+    // Check that each of the cells:
+    // ... have their own associated color value as the background-color
+    // ... have their labels hidden initially
+    // ... has a size that is reasonable given the UIBounds available (how do we define this?)
+  }
+  
+//  func testSelectingAndDeselectingAPrimaryColor() {
+//    
+//    //TODO: do this a few times just for funm
+//    //TODO: modularize code in here such as:
+//    // …testing that the cell is in its 'unselected-state', i.e. label is hidde, background-color is that of associated value, etc.
+//    // …testing that the screen is in a 'selected' or 'unselected' state perhaps – which would in turn use other modularized bits of test code
+//    
+//    // Tap a random cell
+//    //TODO: figure out why we can put numbers in boundBy but not variables??
+////    let visibleCells = XCUIApplication().collectionViews.children(matching: .cell)
+////    let randomCell = randomIntegerBetween0(and: Int(visibleCells.count))
+//    
+//    XCUIApplication().collectionViews.children(matching: .cell).element(boundBy: 4).children(matching: .other).element.tap()
+//    XCTAssertTrue(true)
+//    
+//    //    cells.element(boundBy: randomCell).children(matching: other).element.tap()
+//    
+//    //**** Check that these things (eventually) happen – give a max delay of say 5 seconds
+//    
+//    // Check that the navigation bar:
+//    // …has changed color to the selected cell's color
+//    // …has changed its title to the selected cell's color name
+//    
+//    // Check that the next button:
+//    // …has become enabled
+//    // …has changed its tintColor depending on the chosen color
+//    
+//    // Check that the tapped cell:
+//    // …retains its selected sate
+//    
+//    // Check that all cells except the tapped cell (at least those that are in view):
+//    // …changed their background-color to grey
+//    // …made their title visible, with a white color
+//    
+//    //****
+//  }
   
 //  func testSelectingTheFirstColorFollowedByTheLastColor() {
 //    //do this to mainly invoke a swipe down on the scrollView either implicitiyl implicitly or explicitly
