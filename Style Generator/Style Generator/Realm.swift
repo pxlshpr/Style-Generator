d91616b33d2fae6f6ccdc2ea536c14a26d9807a2
Ahmed Khalaf <pxlshpr@gmail.com>
2017-02-03 22:07:39 +0800

    begin unit tests

---
+++ Style Generator/Style Generator/Realm.swift
@@ -0,0 +1,16 @@
+//
+//  Realm.swift
+//  Style Generator
+//
+//  Created by Ahmed Khalaf on 3/2/17.
+//  Copyright © 2017 Ahmed Khalaf. All rights reserved.
+//
+
+import Foundation
+
+func deleteAllRealmData() {
+  let realm = try! Realm()
+  try! realm.write {
+    realm.deleteAll()
+  }
+}
---
+++ Style Generator/Style GeneratorTests/ColorObjectTests.swift
@@ -0,0 +1,33 @@
+//
+//  ColorObjectTests.swift
+//  Style Generator
+//
+//  Created by Ahmed Khalaf on 3/2/17.
+//  Copyright © 2017 Ahmed Khalaf. All rights reserved.
+//
+
+import XCTest
+
+class ColorObjectTests: XCTestCase {
+  
+  override func setUp() {
+    super.setUp()
+  }
+  
+  override func tearDown() {
+    super.tearDown()
+  }
+  
+  func testColorObjectHasPrimaryKeyOfHex() {
+    //is this redundant?
+  }
+  
+  func testColorObjectIsARealmObject() {
+    
+  }
+  
+  func testColorObjectCanHaveAHueSetUsingAnEnum() {
+    //hmm?
+  }
+  
+}
